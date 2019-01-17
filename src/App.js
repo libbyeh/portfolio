7b9ea39f66a8ec94f2a552c2a8282dfbb6395241
Elizabeth Yeh <esnider1699@gmail.com>
2019-01-17 15:10:04 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.scss';
+
+
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      snapshot: [],
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className='header'>
+          <h1 className='header-title'><span>Elizabeth Yeh</span> Portfolio</h1>
         </header>
+        <section className='portfolio-grid'>
+          <div className='box box1'></div>
+          <div className='box box2'></div>
+          <div className='box box3'></div>
+          <div className='box box4'></div>
+          <div className='box box5'></div>
+          <div className='box box6'></div>
+        </section>
       </div>
     );
   }
